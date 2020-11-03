a7b8bdd965ccf600303de86d3d1f0b88ffb34ead
maliendo106 <maliendo106@alumnos.iua.edu.ar>
2020-11-03 09:44:01 -0300

    se agregaron los archivos comprimidos

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 import java.io.*;
 import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.util.zip.GZIPInputStream;
+
 class Main {
 
     public static void main (String[] args){
+
         List<Testeo> testeos = new ArrayList<>();
-        try{
-            try (BufferedReader br = new BufferedReader(new FileReader("resources/CasosCovid19.txt"))) {
+        String file = "resources/Covid19Casos-1000.txt.gz";
+        try {
+            GZIPInputStream gzip = new GZIPInputStream(new FileInputStream(file));
+            BufferedReader br = new BufferedReader(new InputStreamReader(gzip));
+
                 String line;
+
                 while ((line = br.readLine()) != null) {
                     String[] values = line.split(",");
                     for(int i=0; i < values.length; i ++){
-                        if(values[i].length() > 1){
-                            values[i] = values[i].substring(1, values[i].length() - 1);
+                        if(values[i].length() > 1){    // evito los datos con comas dobles -> ,,
+                            values[i] = values[i].substring(1, values[i].length() - 1);     // acorto el string para que salga sin ""
                         }
 
 
@@ -25,13 +36,10 @@ class Main {
 
                     testeos.add(t);
                 }
-
             }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-
-
+     catch (Exception e) {
+        System.out.println("El archivo no se pudo abrir correctamente! ");
+    }
 
 
     }
